adeecd445a3433e9c30a30b08c1b3dae2353aeda
Nickyi175 <124185028+Nickyi175@users.noreply.github.com>
2023-10-13 15:06:47 -0600

    correcciones

--- src/bancolab/Banco.java
+++ src/bancolab/Banco.java
@@ -1,65 +1,91 @@
 package bancolab;
 
+import javax.swing.JOptionPane;
+
 public class Banco {
-    
+
     private CuentaBancaria[] cuentas;
-    private static final double tasaInteres = 0.05; 
-    private int numCuentas;
+    private static final double tasaInteres = 0.05;
+//    private int numCuentas;
 
     public Banco(int capacidad) {
         cuentas = new CuentaBancaria[capacidad];
-        numCuentas = 0;
+//        numCuentas = 0;
     }
 
-    public CuentaBancaria buscar(int codigo) {
-        for (int i = 0; i < cuentas.length; i++) {
-            if (cuentas[i].getCodigo() == codigo) {
-                return cuentas[i];
+//REVISADO
+    public CuentaBancaria Buscar(int codigo) {
+        for (CuentaBancaria cuenta : cuentas) {
+            if (cuenta != null && cuenta.getCodigo() == codigo) {
+                return cuenta;
             }
         }
         return null;
+//        for (int i = 0; i < cuentas.length; i++) {
+//            if (cuentas!=null&&cuentas[i].getCodigo() == codigo) {
+//                return cuentas[i];
+//            }
+//        }
+//        return null;
     }
+//REVISADO
 
-    public boolean agregar(int codigo, String nombre) {
-        if (numCuentas >= cuentas.length) {
-            return false; 
+    public boolean Agregar(int codigo, String nombre) {
+
+        if (Buscar(codigo) != null) {
+            System.out.println("ya existe el codigo");
+            return false;
         }
 
-        if (buscar(codigo) != null) {
-            return false; 
+        for (int pos = 0; pos < cuentas.length; pos++) {
+            if (cuentas[pos] == null) {
+                CuentaBancaria nuevaCuenta = new CuentaBancaria(codigo, nombre);
+                cuentas[pos] = nuevaCuenta;
+                System.out.println("Se agregó");
+                return true;
+            }
         }
 
-        CuentaBancaria nuevaCuenta = new CuentaBancaria(codigo, nombre);
-        cuentas[numCuentas] = nuevaCuenta;
-        numCuentas++;
-        return true;
+        return false;
     }
 
-    public void transferir(int origen, int destino, double monto) {
-        CuentaBancaria cuentaOrigen = buscar(origen);
-        CuentaBancaria cuentaDestino = buscar(destino);
+    //REVISADO
+    public void Transferir(int CodOrigen, int CodDestino, double monto) {
+        CuentaBancaria cuentaOrigen = Buscar(CodOrigen);
+        CuentaBancaria cuentaDestino = Buscar(CodDestino);
 
-        if (cuentaOrigen == null || cuentaDestino == null) {
-           cuentaOrigen.Retirar(monto) ;
-           cuentaDestino.Depositar(monto);
+        if (cuentaOrigen != null && cuentaDestino != null) {
+            if (cuentaOrigen.Retirar(monto)) {
+                cuentaDestino.Depositar(monto);
+                System.out.println("Se transfirio");
+                JOptionPane.showMessageDialog(null, "¡Transferencia Exitosa!");
+            } else {
+                System.out.println(" no se pudo transferir");
+                JOptionPane.showMessageDialog(null, "¡No se pudo realizar la transferencia!");
+            }
+        } else {
+            System.out.println("no se hallo una o dos de las cuentas");
+            JOptionPane.showMessageDialog(null, "¡No se pudo hallar una o dos de las cuentas!");
         }
     }
 
-    public void calcularIntereses() {
-        for (int i = 0; i < numCuentas; i++) {
-            CuentaBancaria cuenta = cuentas[i];
-            double interesMensual = cuenta.getSaldoCuenta() * tasaInteres;
-            cuenta.Depositar(interesMensual);
+    //REVISADO
+    public void Intereses() {
+        for (CuentaBancaria cuenta : cuentas) {
+            if (cuenta != null) {
+                double intereses = cuenta.getSaldoCuenta() * tasaInteres;
+                cuenta.Depositar(intereses);
+            }
         }
     }
-
-    public void lista(double monto) {
-        for (int i = 0; i < numCuentas; i++) {
-            CuentaBancaria cuenta = cuentas[i];
+    
+    //REVISADO
+    public void Lista(double monto) {
+        for (int pos = 0; pos < cuentas.length; pos++) {
+            CuentaBancaria cuenta = cuentas[pos];
             if (cuenta.getSaldoCuenta() >= monto) {
-                System.out.println(cuenta.toString());
+                cuenta.Print();
             }
         }
     }
 }
-
