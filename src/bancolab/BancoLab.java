ff307c3b0a2ae3a8277de34a0b3c57c0a4cf56dc
Nickyi175 <124185028+Nickyi175@users.noreply.github.com>
2023-10-13 13:55:05 -0600

    CunetaBancaria

--- src/bancolab/BancoLab.java
+++ src/bancolab/BancoLab.java
@@ -1,15 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package bancolab;
 
-/**
- *
- * @author tomea
- */
+import javax.swing.JOptionPane;
+
 public class BancoLab {
 
-   
-    
+    private int codigo;
+    private String name;
+    private double saldoCuenta;
+
+    public BancoLab(int codigo, String name) {
+        this.codigo = codigo;
+        this.name = name;
+        saldoCuenta = 500;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    void Depositar(double monto) {
+        if (monto >= 0) {
+            saldoCuenta += monto;
+        } else {
+            System.out.println("El valor es negativo");
+            JOptionPane.showMessageDialog(null, "El valor es negativo");
+        }
+    }
+
+    boolean Retirar(double monto) {
+        if (monto >= 0 && monto <= saldoCuenta) {
+            saldoCuenta -= monto;
+            return true;
+        }
+        return false;
+    }
+
+    public String Print() {
+
+        return "BancoLab{" + "codigo:" + codigo + ", nombre: " + name + ", saldo: Lps. " + saldoCuenta + '}';
+
+    }
+
 }
